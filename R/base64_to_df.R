432866a23c58b112cc033846935439944dd5824a
reperiam <ulrich.lang@swr.de>
2022-12-14 08:33:28 +0100

    add utf-8 encoding and warning for empty table

--- R/base64_to_df.R
+++ R/base64_to_df.R
@@ -11,5 +11,9 @@
 base64_to_df <- function(x) {
   raw_csv <- rawToChar(base64enc::base64decode(x))
 
-  return(read.csv(textConnection(raw_csv), stringsAsFactors = FALSE, sep = ";"))
+  if (raw_csv == "\r\n") {
+    warning("empty data table", call. = F)
+    return(data.frame())
+  }
+  return(read.csv(textConnection(raw_csv), stringsAsFactors = FALSE, sep = ";", encoding = "UTF-8"))
 }
