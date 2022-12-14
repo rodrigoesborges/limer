5190484a851d1ded9d8734695d42781daa7500c3
reperiam <ulrich.lang@swr.de>
2022-12-14 08:34:34 +0100

    add documentation

--- R/get_responses.R
+++ R/get_responses.R
@@ -1,12 +1,15 @@
 #' Export data from a LimeSurvey survey
 #'
 #' This function exports and downloads data from a LimeSurvey survey.
-#' @param iSurveyID \dots
-#' @param sDocumentType \dots
-#' @param sLanguageCode \dots
-#' @param sCompletionStatus \dots
-#' @param sHeadingType \dots
-#' @param sResponseType \dots
+#' @param iSurveyID string ID of the Survey
+#' @param sDocumentType string any format available by plugins (for example :
+#' pdf, csv, xls, doc, json).
+#' @param sLanguageCode string (optional) The language to be used.
+#' @param sCompletionStatus string (optional) 'complete','incomplete' or 'all'
+#' - defaults to 'all'.
+#' @param sHeadingType string (optional) 'code','full' or 'abbreviated'.
+#' defaults to 'code'.
+#' @param sResponseType (optional)'short' or 'long'. defaults to 'short'
 #' @param \dots Further arguments to \code{\link{call_limer}}.
 #' @export
 #' @examples \dontrun{
@@ -19,7 +22,7 @@ get_responses <- function(iSurveyID, sDocumentType = "csv", sLanguageCode = NULL
   # Put all the function's arguments in a list to then be passed to call_limer()
   params <- as.list(environment())
   dots <- list(...)
-  if(length(dots) > 0) params <- append(params,dots)
+  if (length(dots) > 0) params <- append(params,dots)
   # print(params) # uncomment to debug the params
 
   results <- call_limer(method = "export_responses", params = params)
