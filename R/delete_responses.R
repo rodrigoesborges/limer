37bfecf3d90876669f8bfb8d2db73140abae8342
reperiam <ulrich.lang@swr.de>
2023-01-09 14:15:38 +0100

    delete_responses function

---
+++ R/delete_responses.R
@@ -0,0 +1,48 @@
+#' delete_responses
+#'
+#' Deletes one or more responses
+#'
+#' @param iSurveyID integer, ID of the Survey to insert responses
+#' @param verbose boolean, Giving out logging info
+#' @param iResponseIDs integer, response Ids to be deleted. If not set delete
+#' all.
+#'
+#' @return -
+#' @export
+#'
+
+delete_responses <- function(iSurveyID, iResponseIDs = NULL, verbose = FALSE ) {
+
+  if (!iSurveyID %in% limer::get_survey_list()) {
+    stop(glue::glue("Survey with ID {iSurveyID} does not exist in this installation!"), call. = F)
+  }
+
+  if (is.null(iResponseIDs)) {
+    iResponseIDs <- get_responses(iSurveyID)
+    # Use the first column as ID if there are answers, otherwise set ID to 1
+    iResponseIDs <- ifelse(nrow(iResponseIDs> 0), iResponseIDs %>% dplyr::pull(1), 1)
+
+  }
+
+
+  del_response <- function(x, iSurveyID){
+
+    call_limer("delete_response",
+               params = list("iSurveyID" = iSurveyID,
+                             "iResponseIDs" = x))
+
+  }
+
+  res <- lapply(iResponseIDs, FUN = function(x) del_response(x,iSurveyID ) )
+
+
+  if (any(res %>% unlist() == "Response Id not found") & verbose) {
+    message("one or more Id was not found and could not be deleted")
+  }
+
+  if (all(res %>% unlist() == "deleted") & verbose) {
+    message("Responses successfully deleted")
+  }
+
+}
+
