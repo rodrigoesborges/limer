ec5207964f7f2f95ad19554431675d09bb0adf9f
reperiam <ulrich.lang@swr.de>
2023-05-15 11:22:32 +0200

    Adaptation to missing participants table implemented

--- R/add_participants.R
+++ R/add_participants.R
@@ -13,19 +13,47 @@
 #'
 
 add_participants <- function(iSurveyID, data, bCreateToken = F, chunksize = 200){
-  if (!all(colnames(data) %in% c("token", "firstname", "lastname", "email")) | length(data) != 4 )
-    stop("data must be a dataframe with the columns firstname, lastname, email and token", call. = F)
+
+    if (!exists_participants_table(iSurveyID)) {
+      warning("No participant table found and a new one created", call. = F)
+
+      n <- length(colnames(data))
+      field_names <- glue::glue("description_attribute_{1:n}")
+      fields <- colnames(data)
+      names(fields) = field_names
+      fields <- list(list("attribute_1" = "", description = "blabla"), list("attribute_2" = "", description = "hot"))
+      fields <- list("attribute_1" = "blabla", "attribute_2" = "hot")
+      create_participants_table(iSurveyID, aAttributeFields = fields)
+    }
+
+  # if data is a character vector
+  if (inherits(data, "character")) {
+    data <- data.frame(firstname = "", lastname = "", email = "" , token = data)
+  }
+
+  # For some column types, such as mail addresses, no leading or trailing spaces
+  # may be included.
+  data <- data %>%
+    dplyr::mutate(dplyr::across(dplyr::where(is.character), stringr::str_trim))
+
 
   data <- stats::setNames(split(data, seq(nrow(data))), rownames(data))
   data <- lapply(data, FUN = function(x) unlist(x) %>% as.list() )
 
     # for php memory problems split iLimit in chunks
   if (length(data) > chunksize) {
-    n <- length(data)/chunksize
+    n <- ceiling(length(data)/chunksize)
     limit <- 1
     for (i in 1:n) {
 
-      params <- list("iSurveyID" = iSurveyID, "aParticipantData" = data[limit:(limit + chunksize)], "bCreateToken" = bCreateToken)
+      list_data <- data[limit:(limit + chunksize)]
+      # if the number of list items is smaller than chunksize and this would
+      # result in empty entries, delete empty list items
+      list_data <- Filter(function(x) length(x) > 0, list_data)
+
+      params <- list("iSurveyID" = iSurveyID, "aParticipantData" = list_data,
+                     "bCreateToken" = bCreateToken)
+
       resp <- call_limer(method = "add_participants", params = params)
       resp <- data.table::rbindlist(resp, fill = T) %>% suppressWarnings()
       if ("errors" %in% colnames(resp))
