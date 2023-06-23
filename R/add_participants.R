2b4abfc76922c06c62ed0d2d617ec2c8d102a412
reperiam <ulrich.lang@swr.de>
2023-06-23 10:20:48 +0200

    Bugfix if fewer users than chunksize have been transferred

--- R/add_participants.R
+++ R/add_participants.R
@@ -21,8 +21,6 @@ add_participants <- function(iSurveyID, data, bCreateToken = F, chunksize = 200)
       field_names <- glue::glue("description_attribute_{1:n}")
       fields <- colnames(data)
       names(fields) = field_names
-      fields <- list(list("attribute_1" = "", description = "blabla"), list("attribute_2" = "", description = "hot"))
-      fields <- list("attribute_1" = "blabla", "attribute_2" = "hot")
       create_participants_table(iSurveyID, aAttributeFields = fields)
     }
 
@@ -41,33 +39,35 @@ add_participants <- function(iSurveyID, data, bCreateToken = F, chunksize = 200)
   data <- lapply(data, FUN = function(x) unlist(x) %>% as.list() )
 
     # for php memory problems split iLimit in chunks
+  limit <- 1
   if (length(data) > chunksize) {
     n <- ceiling(length(data)/chunksize)
-    limit <- 1
-    for (i in 1:n) {
-
-      list_data <- data[limit:(limit + chunksize)]
-      # if the number of list items is smaller than chunksize and this would
-      # result in empty entries, delete empty list items
-      list_data <- Filter(function(x) length(x) > 0, list_data)
-
-      params <- list("iSurveyID" = iSurveyID, "aParticipantData" = list_data,
-                     "bCreateToken" = bCreateToken)
-
-      resp <- call_limer(method = "add_participants", params = params)
-      resp <- data.table::rbindlist(resp, fill = T) %>% suppressWarnings()
-      if ("errors" %in% colnames(resp))
-        warning("there were errors when adding participants among others. ", resp$errors[[1]], call. = F)
-      # set count
-      limit <- limit + chunksize + 1
-      cat("\r",round((i/n)*100, digits = 2), "%")
-      utils::flush.console()
-    }
-
+  } else{
+    n <-  1
   }
 
+  for (i in 1:n) {
+
+    list_data <- data[limit:(limit + chunksize)]
+    # if the number of list items is smaller than chunksize and this would
+    # result in empty entries, delete empty list items
+    list_data <- Filter(function(x) length(x) > 0, list_data)
+
+    params <- list("iSurveyID" = iSurveyID, "aParticipantData" = list_data,
+                   "bCreateToken" = bCreateToken)
+
+    resp <- call_limer(method = "add_participants", params = params)
+    resp <- data.table::rbindlist(resp, fill = T) %>% suppressWarnings()
+    if ("errors" %in% colnames(resp))
+      warning("there were errors when adding participants among others. ", resp$errors[[1]], call. = F)
+    # set count
+    limit <- limit + chunksize + 1
+    cat("\r",round((i/n)*100, digits = 2), "%")
+    utils::flush.console()
+  }
 
   cat("\r")
   utils::flush.console()
 
 }
+
