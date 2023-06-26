dc371cbdd91e507ca5d88be616646264d3c6ff7a
reperiam <ulrich.lang@swr.de>
2023-06-26 17:03:37 +0200

    set missing argument iLimit automatically to the highest ID in the participant table

--- R/get_participants.R
+++ R/get_participants.R
@@ -14,12 +14,18 @@
 #' get_participants(12345, iStart=1, iLimit=10, bUnused=FALSE, aAttributes=FALSE)
 #' }
 
-get_participants <- function(iSurveyID, iStart, iLimit, bUnused, aAttributes){
+get_participants <- function(iSurveyID, iStart = 1, iLimit, bUnused, aAttributes){
   # Put all the function's arguments in a list to then be passed to call_limer()
   params <- as.list(environment())
-
+  if (is_missing(iLimit)) {
+    iLimit <- limer::get_participants(iSurveyID, iStart = 1, iLimit = 1000000) %>%
+      dplyr::pull(tid) %>% max() %>% as.numeric()
+    params$iLimit <-  iLimit
+  }
   results <- call_limer(method = "list_participants", params = params)
-  if (is.list(results))
+  if (!inherits(results, "data.frame"))
     warning(results, call. = FALSE)
+
+
   return(results)
 }
