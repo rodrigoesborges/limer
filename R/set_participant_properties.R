9662c6de1b7a381212956aafa978239602451b38
reperiam <ulrich.lang@swr.de>
2023-08-17 14:07:16 +0200

    Feature set_participant_properties

---
+++ R/set_participant_properties.R
@@ -0,0 +1,38 @@
+#' set_participant_properties
+#'
+#' Allow to set properties about a specific participant, only one particpant can
+#' be updated.
+#'
+#' @param iSurveyID integer, ID of the Survey to insert responses
+#' @param aTokenQueryProperties integer, tid of the user
+#' @param aTokenData list, key-value-pair of attribute name an value
+#'
+#' @return none
+#' @export
+#'
+
+set_participant_properties <- function(iSurveyID, aTokenQueryProperties,
+                                       aTokenData){
+
+      max_id <- max(as.numeric(aTokenQueryProperties))
+
+      aTokenIDs_in_survey <- get_participants(iSurveyID, iStart = 1, iLimit = max_id) %>%
+        dplyr::pull(tid)
+
+      if (!all(aTokenQueryProperties %>% unlist() %in% aTokenIDs_in_survey))
+        warning("some Tid in the aTokenData not found in participants table", call. = F)
+
+      n <- length(aTokenQueryProperties)
+
+      for (i in 1:n) {
+        params <- list("iSurveyID" = iSurveyID,
+                       "aTokenQueryProperties" = aTokenQueryProperties[i],
+                       aTokenData = aTokenData)
+
+
+        resp <- call_limer(method = "set_participant_properties", params = params)
+
+      }
+      message(glue::glue("{i} entrie(s) edited"))
+
+}
