62832b30b1a83354eb844ed527a026c4e54fd8ed
reperiam <ulrich.lang@swr.de>
2023-08-17 15:41:11 +0200

    implement param sUsername

--- R/get_questions_properties.R
+++ R/get_questions_properties.R
@@ -4,21 +4,20 @@
 #' Generates a dataframe with the properties of all questions in a survey
 #'
 #' @param iSurveyID integer, ID of the Survey to insert responses
+#' @param sUsername character, Username in Limesurvey installation
 #' @param verbose boolean, Giving out logging info
 #'
 #' @return dataframe
 #' @export
 #'
 
-get_questions_properties <- function(iSurveyID, verbose = FALSE ) {
+get_questions_properties <- function(iSurveyID, sUsername = NULL, verbose = FALSE ) {
 
-  if (!iSurveyID %in% limer::get_survey_list()) {
+  if (!iSurveyID %in% limer::get_survey_list(sUsername = sUsername)) {
     stop(glue::glue("Survey with ID {iSurveyID} does not exist in this installation!"), call. = F)
   }
 
   res <- limer::call_limer("list_questions", params = list("iSurveyID" = iSurveyID))
-
-  df <- data.table::rbindlist(res, fill = T) %>% suppressWarnings()
   return(df)
 }
 
